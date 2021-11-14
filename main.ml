77904744773211e12f1f1ce36106695e8e38d432
Roi Santos Ríos <roi.santos.rios@gmail.com>
2021-11-14 12:59:19 +0100

    Now it reads multiple lines

--- main.ml
+++ main.ml
@@ -6,13 +6,38 @@ open Lambda;;
 open Parser;;
 open Lexer;;
 
+open String;;
+open List;;
+
+(*Simple function that uses regex to check if a string contains any given expression.
+  In our case, we will only use this to check if a string contains the stop character ;*)
+let has_char str c =
+  try match (Str.search_forward (Str.regexp c) str 0) with _-> true  
+    with Not_found -> false
+;;
+
+(*This function reads the console input, concatenating each line (string) it reads with
+  a space in between each, and it only stops when it finds a ;. The remaining input to the
+  right of the ; gets ignored.*)
+let read_input () =
+  let rec aux acc =
+    let line = read_line () in
+    if (has_char line ";") then 
+      let split_line = (hd (split_on_char ';' line)) in 
+        cat acc (cat " " split_line)
+    else 
+      aux (cat acc (cat " " line))
+  in aux ""
+;;
+  
+(*We changed the read_line funtion for our own read_input one.*)
 let top_level_loop () =
   print_endline "Evaluator of lambda expressions...";
   let rec loop ctx =
     print_string ">> ";
     flush stdout;
     try
-      let tm = s token (from_string (read_line ())) in
+      let tm = s token (from_string (read_input ())) in
       let tyTm = typeof ctx tm in
       print_endline (string_of_term (eval tm) ^ " : " ^ string_of_ty tyTm);
       loop ctx
