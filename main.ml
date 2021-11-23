77230edaa9db30a092e670ca5f900e098753d045
Roi Santos Ríos <roi.santos.rios@gmail.com>
2021-11-23 20:23:07 +0100

    added flex operator and read from file

--- main.ml
+++ main.ml
@@ -29,6 +29,52 @@ let read_input () =
       aux (cat acc (cat " " line))
   in aux ""
 ;;
+
+(*This function reads the file that we input in the command line, and does essentialy the same as
+  read_input. The only difference is that this one reads from the input channel of a file, and also
+  it returns a tuple, containing both the desired input string and the number of the line where
+  it started to read the sentence that contains the error.*)
+let read_input_from_file in_channel numLine =
+  let rec aux acc lineNum =
+    let line = input_line in_channel in
+    if (has_char line ";") then 
+      let split_line = (hd (split_on_char ';' line)) in 
+        ((cat acc (cat " " split_line)),lineNum+1)
+    else 
+      aux (cat acc (cat " " line)) (lineNum+1)
+  in aux "" numLine
+;;
+
+(*This loop is used to read the contents of the file. Its changed so it stops when it reads a whole
+  sentence (finishing with a ';') and also displays the line where the error was found*)
+let file_loop () = 
+  let in_channel = open_in Sys.argv.(1) in
+  try
+    while true do
+      let rec loop ctx numLine =
+        try
+          let inp = read_input_from_file in_channel numLine in
+          let tm = s token (from_string (fst inp)) in
+          let tyTm = typeof ctx tm in
+          print_endline (string_of_term (eval tm) ^ " : " ^ string_of_ty tyTm);
+          loop ctx (snd inp)
+        with
+           Lexical_error ->
+             print_endline ("lexical error in line: " ^ string_of_int numLine);
+             exit 0;
+         | Parse_error ->
+             print_endline ("syntax error in line: " ^ string_of_int numLine);
+             exit 0;
+         | Type_error e ->
+             print_endline ("type error: " ^ e ^ " in line: " ^ string_of_int numLine);
+             exit 0;
+      in
+        loop emptyctx 1
+    done
+  with End_of_file ->
+    close_in in_channel
+  ;;
+;;
   
 (*We changed the read_line funtion for our own read_input one.*)
 let top_level_loop () =
@@ -57,6 +103,9 @@ let top_level_loop () =
     loop emptyctx
   ;;
 
-top_level_loop ()
+if (Array.length Sys.argv >= 2) then
+  file_loop ()
+else
+  top_level_loop ()
 ;;
 
